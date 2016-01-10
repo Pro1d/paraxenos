d5f68818329da65529fa0a320d8366a4133d546b
Pro1d <topaffaire_pg@hotmail.fr>
2016-01-10 20:16:37 +0100

    Switch automatically to state game after 3sec

--- Paraxenos/src/fr/insa/clubinfo/paraxenos/game/state/StateMenu.java
+++ Paraxenos/src/fr/insa/clubinfo/paraxenos/game/state/StateMenu.java
@@ -1,11 +1,19 @@
 package fr.insa.clubinfo.paraxenos.game.state;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.graphics.Canvas;
+
+import com.fbessou.sofa.GameIOHelper;
+import com.fbessou.sofa.GameIOHelper.GamePadInputEvent;
+import com.fbessou.sofa.sensor.Sensor;
+
 import fr.insa.clubinfo.paraxenos.game.Game;
 import fr.insa.clubinfo.paraxenos.game.GameState;
 
 public class StateMenu extends GameState {
-
+	
 	public StateMenu(Game g) {
 		super(g);
 		
@@ -13,13 +21,31 @@ public class StateMenu extends GameState {
 
 	@Override
 	public void enter() {
-		game.pushState(new StateGame(game));
+		Timer t= new Timer();
+		t.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				if(game.getIO().isConnected() == false)
+					game.pushState(new StateGame(game));
+			}
+		}, 3000);
 	}
 
 	@Override
 	public void update(Canvas canvas, int frameDelay) {
-		// TODO Auto-generated method stub
-
+		GameIOHelper io = game.getIO();
+		GamePadInputEvent gpEvent;
+		while((gpEvent = io.pollInputEvent()) != null) {
+			switch(gpEvent.event.eventType) {
+			case KEYDOWN:
+				if(gpEvent.event.inputId == Sensor.KEY_CATEGORY_VALUE+1) {
+					game.pushState(new StateGame(game));
+				}
+				break;
+			default:
+				break;
+			}
+		}
 	}
 
 	@Override
