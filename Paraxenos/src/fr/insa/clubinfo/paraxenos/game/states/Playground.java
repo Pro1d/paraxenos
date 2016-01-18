d0bb97b30158f117c25c3cbf47e2bf941355df23
Pro1d <topaffaire_pg@hotmail.fr>
2016-01-18 12:14:07 +0100

    Base state

---
+++ Paraxenos/src/fr/insa/clubinfo/paraxenos/game/states/Playground.java
@@ -0,0 +1,53 @@
+package fr.insa.clubinfo.paraxenos.game.states;
+
+import fr.insa.clubinfo.paraxelib.game.Game;
+import fr.insa.clubinfo.paraxelib.game.State;
+import fr.insa.clubinfo.paraxelib.graphics.Renderer;
+import fr.insa.clubinfo.paraxelib.physics.Engine;
+import fr.insa.clubinfo.paraxenos.entities.Map;
+
+public class Playground extends State {
+
+	private static final String MAP_LAYOUT = "MAP_LAYOUT";
+	private Map map;
+	private Engine physicEngine;
+	
+	public Playground(Game g) {
+		super(g);
+
+	}
+
+	@Override
+	public void enter() {
+		map = new Map(game.getSize());
+		Renderer renderer = game.getRenderer();
+		renderer.addLayoutTop(MAP_LAYOUT);
+		renderer.addDrawable(map, MAP_LAYOUT);
+		physicEngine = new Engine();
+	}
+
+	@Override
+	public void update(int frameDelay) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onActivityPaused() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onActivityResumed() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void leave() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
